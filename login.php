a6198553dbfa588971fd0245dc3b8325ddaf6ebe
Alan Teves Retubis <alanretubis@gmail.com>
2023-09-01 09:22:02 +0800

    :tada: For Hands On Activity

--- login.php
+++ login.php
@@ -54,57 +54,6 @@ if(Input::exists()){
   <title><?php echo strtoupper($page_title); ?> | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <script type="text/javascript">
-  
-window.onload = initClock;
- 
-function initClock() {
-  var now = new Date();
-  var hr  = now.getHours();
-  var min = now.getMinutes();
-  var sec = now.getSeconds();
-  if (min < 10) min = "0" + min;  // insert a leading zero
-  if (sec < 10) sec = "0" + sec;
-
-  var suf;
-  if(hr > 11)
-  {
-    suf = "PM";
-  }
-  else
-  {
-  suf = "AM";
-  }
-  if(hr > 12){
-  document.getElementById('time').innerHTML
-        = hr%12 + ":" + min + ":" + sec + " "+suf;
-    }else
-    {
-       document.getElementById('time').innerHTML
-        = hr + ":" + min + ":" + sec +" "+ suf;
-    }
-  setTimeout('initClock()', 500);
-} 
-
-</script>
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -113,13 +62,6 @@ function initClock() {
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <h4 class="login-box-msg" style="float:left; margin-left:30px; margin-top:16px; color:red">
-      <b> 
-        <?php 
-          echo date_create("",timezone_open("Asia/Manila"))->format("F d, Y");
-        ?>
-      </b>| <span id="time" /></span>
-    </h4> 
     <p class="login-box-msg">Please sign in to start</p>
     <form method="post">
       <div class="form-group has-feedback">
--- register.php
+++ register.php
@@ -3,26 +3,74 @@ require_once 'core/init.php';
 
 try{
 
-$user = new User();
-$salt = Hash::salt(32);
+    if(Input::exists()){
+        if(Token::check(Input::get('token'))){
+            $user = new User();
+            $salt = Hash::salt(32);
 
-try{
+            try{
 
-    $user->create(array(
-        'username' => 'johndoe',
-        'password' => Hash::make('password', $salt),
-        'salt' => $salt,
-        'first_name' => 'John',
-        'surname' => 'Doe',
-        'active' => 1
-    ));
+                $user->create(array(
+                    'username' => 'johndoe',
+                    'password' => Hash::make('password', $salt),
+                    'salt' => $salt,
+                    'first_name' => 'John',
+                    'surname' => 'Doe',
+                    'active' => 1
+                ));
 
-}catch(Exception $e){
-    die($e->getMessage());
-}
+            }catch(Exception $e){
+                die($e->getMessage());
+            }
 
-Redirect::to('index.php');
+            Redirect::to('index.php');
+        }
+    }
 
 }catch(Exception $ex){
 	echo $ex->getMessage();
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="post">
+            <div class="container">
+                <h1>Sign Up</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr>
+                <input type = "hidden" name ="token" value ="<?php echo Token::generate(); ?>">
+                <label for="username"><b>Username:</b></label><br>
+                <input type="text" placeholder="Enter Username" name="username" required>
+                <br>
+
+                <label for="firstname"><b>Firstname:</b></label><br>
+                <input type="text" placeholder="Enter Firstname" name="firstname" required>
+                <br>
+
+                <label for="surname"><b>Surname:</b></label><br>
+                <input type="text" placeholder="Enter Surname" name="surname" required>
+                <br>
+
+                <label for="psw"><b>Password:</b></label><br>
+                <input type="password" placeholder="Enter Password" name="password" required>
+                <br>
+
+                <label for="psw-repeat"><b>Repeat Password:</b></label><br>
+                <input type="password" placeholder="Repeat Password" name="confirm-password" required>
+                <br>
+
+                <div class="clearfix">
+                <button type="button" class="cancelbtn">Cancel</button>
+                <button type="submit" class="signupbtn">Sign Up</button>
+                </div>
+            </div>
+        </form>
+    </body>
+</html>
